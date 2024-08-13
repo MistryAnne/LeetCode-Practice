804814a870c4dbb4c96a5ec1e74a737a09d35469
AMistry <134707823+MistryAnne@users.noreply.github.com>
2024-08-12 20:23:55 -0400

    Time: 865 ms (17.26%) | Memory: 0B (100.00%) - LeetSync

---
+++ SQL/1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -0,0 +1,7 @@
+/* Write your PL/SQL query statement below */
+Select TO_CHAR(t.trans_date, 'YYYY-MM') as "month", t.country as "country", Count(*) as "trans_count", Count(a.id) as "approved_count", Sum(t.amount) as "trans_total_amount", NVL(sum(a.amount), 0) as "approved_total_amount"
+from Transactions t full join (Select id, amount, country, trans_date
+from Transactions 
+where state = 'approved' ) a
+on t.trans_date = a.trans_date and t.id = a.id
+Group by t.country, TO_CHAR(t.trans_date, 'YYYY-MM'); 
