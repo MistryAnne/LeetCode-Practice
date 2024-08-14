63a5da7e63f2de551d229f653710ee11b0e24021
AMistry <134707823+MistryAnne@users.noreply.github.com>
2024-08-14 13:50:32 -0400

    Time: 56 ms (72.74%) | Memory: 61.2 MB (29.34%) - LeetSync

--- CSharp/151-reverse-words-in-a-string/reverse-words-in-a-string.cs
+++ CSharp/151-reverse-words-in-a-string/reverse-words-in-a-string.cs
@@ -2,12 +2,12 @@ public class Solution {
     public string ReverseWords(string s) {
         string[] listString = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
         int length = listString.Length - 1;
-        string newStr = "";
-        for(int i = length; i >= 0; i--){
+        string newStr = listString[length];
+        for(int i = length -1; i >= 0; i--){
             if(listString[i] != " "){
                 newStr += " " +listString[i];
             }
         }
-        return newStr.Substring(1);
+        return newStr;
     }
 }
